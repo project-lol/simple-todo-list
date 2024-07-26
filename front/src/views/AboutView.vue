fdd80b27696fb79ff83eeac015649ac625fb0877
노예찬 <71473074+noy3928@users.noreply.github.com>
2024-07-26 09:41:04 +0900

    feat : study until step 7

--- front/src/views/AboutView.vue
+++ front/src/views/AboutView.vue
@@ -3,6 +3,7 @@ import { ref } from 'vue'
 const text = ref('hello world')
 const titleClass = ref('title')
 const count = ref(0)
+const awesome = ref(false)
 
 const changeTitleColor = () => {
   titleClass.value = 'title-blue'
@@ -10,6 +11,10 @@ const changeTitleColor = () => {
 
 const incrementCount = () => {
   count.value++
+}
+
+const toggleAwesome = () => {
+  awesome.value = !awesome.value
 }
 </script>
 
@@ -21,6 +26,12 @@ const incrementCount = () => {
 
   <br />
   <input v-model="text" />
+
+  <p v-if="awesome">Awesome is true</p>
+  <p v-else>Awesome is false</p>
+  <button @click="toggleAwesome">Toggle awesome</button>
+
+  <!-- // https://vuejs.org/tutorial/#step-7 -->
 </template>
 
 <style>
